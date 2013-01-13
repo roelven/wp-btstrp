ae128eaf4b5d0372c6c53e84ab30b073a39ab4c9
Roel van der Ven <roel@soundcloud.com>
2013-01-13 23:47:12 +0100

    add wp-content dir + some suggested plugins

---
+++ wp-content/plugins/maintenance-mode/maintenance-mode_theme_default.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php bloginfo('name'); ?></title>
+</head>
+<body>
+
+  <div style="width: 400px; margin: 120px auto">
+    <!-- Begin MailChimp Signup Form -->
+    <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+    	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    </style>
+    <div id="mc_embed_signup">
+    <form action="http://blomandblom.us4.list-manage.com/subscribe/post?u=c1e8ce40c80ce818d57aea1a6&amp;id=896bb4a7cf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+    	<label for="mce-EMAIL">Sign up to stay up to date of Blom &amp; Blom</label>
+
+    	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </form>
+    </div>
+
+    <!--End mc_embed_signup-->
+    
+  </div>
+  
+</body>
+</html>
